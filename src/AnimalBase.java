88161c0e30b1fac8523341f3e73c230df6166de4
Veronica <frida-nanni@hotmail.com>
2022-09-12 22:37:58 +0200

    second commit

--- src/AnimalBase.java
+++ src/AnimalBase.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AnimalBase {
 
@@ -8,12 +12,12 @@ public class AnimalBase {
         animals = new ArrayList<>();
     }
 
-    public void start() {
+    public void start() throws FileNotFoundException {
         UserInterface ui = new UserInterface(this);
         ui.start();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         AnimalBase app = new AnimalBase();
         app.start();
     }
@@ -57,12 +61,48 @@ public class AnimalBase {
     }
 
 
-    public void loadDatabase() {
-        System.err.println("LOAD not yet implemented!");
+    public void loadDatabase() throws FileNotFoundException {
+        Scanner fileScanner = new Scanner(new File("animals.csv")); //der bruges to scanner objekter til at læse
+        while (fileScanner.hasNextLine()) { //fra filen, en til at læse linjer fra filen og anden til at læse hver linje
+            Scanner sc = new Scanner(System.in).useDelimiter(";"); //laver tokens mellem ;, name er token, description er,
+            String name = fileScanner.next(); //next() bruges når man kun vil læse til delimiter ; og ikke resten af linjen
+            String description = fileScanner.next();
+            String type = fileScanner.next();
+            //System.out.println(); //scanner bug skal ikke bruges da den kun bruges ved brugerinput
+            int age = fileScanner.nextInt();
+            double weight = fileScanner.nextDouble();
+
+            Animal animal = new Animal(name,description,type,age,weight);
+
+            sc.close();
+        }
+
+        String line = fileScanner.nextLine();
+        Scanner lineScanner = new Scanner(line).useDelimiter(";");
+
     }
 
-    public void saveDatabase() {
-        System.err.println("SAVE not yet implemented!");
+    public void saveDatabase() throws FileNotFoundException {
+        PrintStream out = new PrintStream("animals.csv");
+
+        for(Animal animal : animals) {
+            writeAnimal(out, animal);
+        }
+    }
+
+    public void writeAnimal (PrintStream out, Animal animal){
+        out.print(animal.getName());
+        out.print(";");
+        out.print(animal.getDesc());
+        out.print(";");
+        out.print(animal.getType());
+        out.print(";");
+        out.print(animal.getAge());
+        out.print(";");
+        out.print(animal.getWeight());
+        out.print("\n");
+
+
     }
 
 }
